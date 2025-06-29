da3442e7b03c33035aabb61e8ab3aa6c181201b6
Filippo Moscatelli <filipp28mo@gmail.com>
2025-06-29 18:03:40 +0200

    Update db-viewer.js

--- server/routes/db-viewer.js
+++ server/routes/db-viewer.js
@@ -122,21 +122,21 @@ router.get('/dependencies/:table', async (req, res) => {
     try {
         const depsResult = await db.query(`
             SELECT
-    tc.constraint_name,
-    tc.table_name,
-    kcu.column_name
-FROM
-    information_schema.table_constraints AS tc
-    JOIN information_schema.key_column_usage AS kcu
-      ON tc.constraint_name = kcu.constraint_name
-      AND tc.table_schema = kcu.table_schema
-    JOIN information_schema.constraint_column_usage AS ccu
-      ON ccu.constraint_name = tc.constraint_name
-      AND ccu.table_schema = tc.table_schema
-WHERE
-    tc.constraint_type = 'FOREIGN KEY'
-    AND ccu.table_name = 'fleet'
-    AND ccu.column_name = 'id';
+                tc.constraint_name,
+                tc.table_name,
+                kcu.column_name
+            FROM
+                information_schema.table_constraints AS tc
+                JOIN information_schema.key_column_usage AS kcu
+                  ON tc.constraint_name = kcu.constraint_name
+                  AND tc.table_schema = kcu.table_schema
+                JOIN information_schema.constraint_column_usage AS ccu
+                  ON ccu.constraint_name = tc.constraint_name
+                  AND ccu.table_schema = tc.table_schema
+            WHERE
+                tc.constraint_type = 'FOREIGN KEY'
+                AND ccu.table_name = $1
+                AND ccu.column_name = 'id';
         `, [table]);
         res.json(depsResult.rows);
     } catch (err) {
